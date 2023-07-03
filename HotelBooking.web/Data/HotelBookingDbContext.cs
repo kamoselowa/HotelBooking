d3450b8c5f992689c1ff29c01e9ee3d60c7b8fe6
DSelowa <DSelowa@OKITL7TWNGS3>
2023-07-04 01:11:36 +0200

    Add project files.

---
+++ HotelBooking.web/Data/HotelBookingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelBooking.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.web.Data
+{
+    public class HotelBookingDbContext : DbContext
+    {
+        public HotelBookingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost>BlogPosts { get; set; }
+
+        public DbSet <Tag> Tags { get; set; }
+    }
+}
---
+++ HotelBooking.web/Models/Domain/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBooking.web.Models.Domain
+{
+    public class BlogPost
+   {
+        
+        
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public bool Visible{ get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
